bb45bba825255c9e07734f9ed27d55e707efda4b
Nakkia Roberts <nakkiasr@live.com>
2021-09-02 23:33:47 -0700

    Added all of the starting components and ran/tested the GET and POST requests in Postman. Working on GET by ID.

--- music_project/settings.py
+++ music_project/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j!$$+44t&t+38h09@%re9cm3=8lu$w)5vzv3*y6ks*tx)rh$um'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'music'
 ]
 
 MIDDLEWARE = [
@@ -70,15 +70,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'music_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -123,3 +115,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from music_project.local_settings import * 
+except ImportError:
+    pass
