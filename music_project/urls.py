824e933fc819f57e43bf771141fd17115ab67ade
Nakkia Roberts <nakkiasr@live.com>
2021-09-10 08:49:04 -0700

    Added the export file and deleted the extra delete url.

--- music_project/urls.py
+++ music_project/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('music.urls')),
-    path('delete/<int:pk>', include('music.urls'))
 ]
